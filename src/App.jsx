5a634f5dcd29c0d5a9186d60c7a1faf386c7fed5
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-08-18 17:08:55 -0700

    build card section w state placement

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Data from './components/Data';
 import HeroCards from './components/HeroCards';
 
 function App() {
-    const [randData, setRandData] = useState(randomizeData(Data()));
+    const [heroData, setHeroData] = useState([]);
 
-    function randomizeData(heroData) {
-        for (let i = heroData.length - 1; i > 0; i--) {
+    useEffect(() => {
+        const heroIds = [107, 275, 309, 480, 522, 567, 579, 638, 720];
+        let mounted = true;
+
+        const fetchData = async (heroId) => {
+            const response = await fetch('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/' + heroId + '.json');
+            const data = await response.json();
+            return data;
+        };
+
+        let temp = [];
+        heroIds.forEach(heroId => {
+            if (mounted) {
+                temp.push(fetchData(heroId));
+            }  
+        });
+        setHeroData(temp);
+
+        return () => mounted = false;
+        
+    }, []);
+
+    //need to properly set the random data onload
+    function randomizeData(data) {
+        for (let i = data.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
-            let temp = heroData[i];
-            heroData[i] = heroData[j];
-            heroData[j] = temp;
+            let temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
         }
-        return heroData;
+        return data;
     }
+
+    //window.onload = () => {setHeroData(randomizeData(heroData))}
 
     return (
         <>
@@ -22,7 +47,7 @@ function App() {
                 <h1>Super Memory Game</h1>
                 <div /* Counter will go here */ />
             </div>
-            <HeroCards heroData={randData}/>
+            <HeroCards heroData={heroData}/>
         </>
     );
 }
--- src/components/HeroCards.jsx
+++ src/components/HeroCards.jsx
@@ -1,13 +1,15 @@
 import Card from "./Card"
 
 export default function HeroCards({heroData}) {
+    const cards = heroData.map(hero => {
+        return (
+            <Card key={hero.id} heroName={hero.name} heroImg={hero.images.md}/>
+        )
+    });
+
     return (
         <section className="card-section" aria-label="hero cards">
-            {heroData.map(hero => {
-                return (
-                    <Card key={hero.id} heroName={hero.name} heroImg={hero.images.md}/>
-                )
-            })}
+            {cards}
         </section>
     )
 }

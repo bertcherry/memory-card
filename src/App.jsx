a0d65e0719bb15199f00ef714428d7e8f1da0093
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-08-21 10:47:02 -0700

    fix app card loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import Data from './components/Data';
 import HeroCards from './components/HeroCards';
 
 function App() {
     const [heroData, setHeroData] = useState([]);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     useEffect(() => {
         const heroIds = [107, 275, 309, 480, 522, 567, 579, 638, 720];
         let mounted = true;
+        let temp = [];
 
         const fetchData = async (heroId) => {
             const response = await fetch('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/' + heroId + '.json');
             const data = await response.json();
-            return data;
+            temp.push(data);
         };
-
-        let temp = [];
+        
         heroIds.forEach(heroId => {
             if (mounted) {
-                temp.push(fetchData(heroId));
+                fetchData(heroId);
             }  
         });
         setHeroData(temp);
@@ -28,7 +28,6 @@ function App() {
         
     }, []);
 
-    //need to properly set the random data onload
     function randomizeData(data) {
         for (let i = data.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
@@ -39,7 +38,9 @@ function App() {
         return data;
     }
 
-    //window.onload = () => {setHeroData(randomizeData(heroData))}
+    function handleStart() {
+        setIsPlaying(true);
+    }
 
     return (
         <>
@@ -47,7 +48,14 @@ function App() {
                 <h1>Super Memory Game</h1>
                 <div /* Counter will go here */ />
             </div>
-            <HeroCards heroData={heroData}/>
+            {isPlaying === false ? (
+                <div className='info'>
+                    <p>Click each picture only once! The heroes are always on the move and will reshuffle between each click. Try to get all nine. Good luck!</p>
+                    <button onClick={handleStart}>Play</button>
+                </div>
+            ) : (
+                <HeroCards heroData={randomizeData(heroData)}/>
+            )}
         </>
     );
 }
